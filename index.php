626d7664ce5018a38d57050c7d9cfe273f933f25
wongpiwat <wongpiwat153@hotmail.com>
2018-02-13 21:55:45 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>The Event</title>
+</head>
+<body>
+    <div class="title">
+        <span class="title-text">The Event</span>
+        <span style="float:right;font-size: 20px;padding-top:1%">Sign in / Sign up</span>
+    </div>
+    <div class="body">
+        <div class="row">
+            <p></p>
+        </div>
+    </div>
+
+    <div class="body">
+        <div class="row">
+            <div class="col-4">
+                <span class="body-text">Recommended Events</span>
+            </div>
+            <div class="col-4">
+                <span class="body-text">Upcoming Events</span>
+            </div>
+            <div class="col-4">
+            <span class="body-text">Nearby Events</span>
+            </div>
+        </div>
+    </div>
+
+        <div class="finder">
+            <span class="body-text">Finder</span>
+        <div class="row">
+            <div class="col-4">
+                <p>Business</p>
+                <p>Education</p>
+                <p>Family</p>
+            </div>
+            <div class="col-4">
+                <p>Health</p>
+                <p>Hobbies</p>
+                <p>Science Technology</p>
+            </div>
+            <div class="col-4">
+                <p>Travel</p>
+                <p>Sports</p>
+                <p>Food</p>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+
+    <br>
+        <p><b>&copy; Designed by Kiticha Garden</b></p>
+        <br>
+    </footer>
+</body>
+
+</html>
