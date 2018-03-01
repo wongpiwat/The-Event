029e86ed9d45cc8869d3ea03f8086f7a90fc19d1
sunvsp <31379663+sunvsp@users.noreply.github.com>
2018-03-02 00:00:37 +0700

    can login with email address

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -13,6 +13,41 @@ class Database {
     }
 
 
+    function readAccount ($username,$password){
+        $statement = $this->connect->query('SELECT * FROM account WHERE username='."'".$username."'".' and password='."'".$password."'");
+        $statement->setFetchMode(PDO::FETCH_CLASS, 'Account'); 
+        $account  = $statement->fetch();
+        return $account;
+    }
+
+    function checkAccount($username,$email){
+        $statement = $this->connect->prepare('SELECT username,email FROM account WHERE username=:username or email=:email');
+        $statement->execute([':username' => $username , ':email' => $email]);
+        $result = $statement->fetch(PDO::FETCH_BOTH);
+        if($result["username"] == "" && $result["email"] == ""){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function findUser($username,$password){
+        $statement = $this->connect->prepare('SELECT username,password,email FROM account WHERE (username=:username and password=:password) or (email=:username and password=:password)');
+        $statement->execute([':username' => $username , ':password' => $password]);
+        $result = $statement->fetch(PDO::FETCH_BOTH);
+        if(($result["username"] == $username && $result["password"] == $password) || ($result["email"] == $username && $result["password"] == $password )) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function createAccount($username){
+
+    }
+
+
+
 
     function getConnect(){
         return $this->connect;
