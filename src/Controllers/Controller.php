0b4ab89d3464b51e5912d49b0f4fd2a01bca6315
sunvsp <31379663+sunvsp@users.noreply.github.com>
2018-03-01 17:38:12 +0700

    add By sunny

---
+++ src/Controllers/Controller.php
@@ -0,0 +1,58 @@
+<?php
+namespace KittichaiGarden\Controllers;
+use KittichaiGarden\Database\Database;
+use PDO;
+class Controller{
+
+    private $database;
+    //constructor
+    function __construct(){
+      $this->database = new Database("3306","kittichai_garden","guest","");
+      echo "helloSun+++++++++++";
+      
+    }
+
+    function login($username,$password){
+      // echo "<br>";
+      // echo $username,$password;
+      // echo "<br>";
+    
+      $connect= $this->database->getConnect();
+      $statement = $connect->query('SELECT * FROM account WHERE username='."'".$username."'".' and password='."'".$password."'");
+      //var_dump($statement);
+      $row = $statement->fetch(PDO::FETCH_BOTH);
+      //echo $row["username"]." ".$row["password"]."-----";
+      
+      //------------------------------------
+      //check if has user in database
+      if($row["username"] == "" && $row["password"] == ""){
+        echo "false";
+      }else{
+        echo "true";
+      }
+
+    }
+
+    function getDatabase(){
+        return $this->database;
+    }
+
+    function setDatabase($d){
+        $this->database = $d;
+    }
+
+
+
+
+    // //test function นะครับ
+    // public function test () {
+    //     echo __METHOD__, PHP_EOL;
+    // }
+
+
+    
+
+
+}
+
+?>
