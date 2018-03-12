fc63823369dae39f9be8afc16de8b681693de2f3
sunvsp <31379663+sunvsp@users.noreply.github.com>
2018-03-12 19:38:35 +0700

    Add files via upload

--- upload-files/index.php
+++ upload-files/index.php
@@ -1,5 +1,8 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
 require('UploadHandler.php');
+// include '../header.php';
 $upload_handler = new UploadHandler();
+// $upload_handler->setUsername($username);
+// echo $upload_handler->getUsername();
 ?>
